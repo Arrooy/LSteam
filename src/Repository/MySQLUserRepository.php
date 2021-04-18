8d830001d3af979b2094ffe50a114bbf77b11712
Miquel Saula Mañosa <miquel.saula@students.salle.url.edu>
2021-04-18 20:17:48 +0200

    added database functionalities

--- src/Repository/MySQLUserRepository.php
+++ src/Repository/MySQLUserRepository.php
@@ -41,10 +41,11 @@ final class MysqlUserRepository implements UserRepository
         return (int)$res['id'];
     }
     
-    // Mira si un usuari existeix basant-se en el email. 
-    public function exists(User $user) : bool{
+    // Mira si un usuari existeix a la taula d'usuraris
+    // verificats basant-se en el email.
+    public function emailExists(User $user) : bool{
         $query = <<< 'QUERY'
-        SELECT * FROM User WHERE email=:email
+        SELECT * FROM users WHERE email=:email
         QUERY;
         $email = $user->email();
         $statement = $this->database->connection()->prepare($query);
@@ -56,9 +57,47 @@ final class MysqlUserRepository implements UserRepository
         return $res != false;
     }
 
+    // Mira si un usuari existeix a la taula d'usuraris
+    // verificats basant-se en el username.
+    public function usernameExists(User $user) : bool{
+        $query = <<< 'QUERY'
+        SELECT * FROM users WHERE email=:email
+        QUERY;
+        $email = $user->email();
+        $statement = $this->database->connection()->prepare($query);
+        $statement->bindParam('email', $email, PDO::PARAM_STR);
+
+        $statement->execute();
+        $res = $statement->fetch();
+
+        return $res != false;
+    }
+
+    // Mira si un token existeix en la taula de pending users
+    public function getPendingUser(string $token) : ?User{
+        $query = <<< 'QUERY'
+        SELECT * FROM usersPending WHERE token=:token
+        QUERY;
+        $statement = $this->database->connection()->prepare($query);
+        $statement->bindParam('token', $token, PDO::PARAM_STR);
+
+        $statement->execute();
+        $res = $statement->fetch();
+
+        if ($res != false) return NULL;
+
+        return new User(
+            $res['username'],
+            $res['email'],
+            $res['password'],
+            $res['birthday'],
+            $res['email'],
+        );
+    }
+
     public function saveUser(User $user): void {
         // Check if user already exists.
-        if($this->exists($user)) {
+        if($this->emailExists($user)) {
             throw new Exception('This email is already used!');
         }
 
@@ -67,6 +106,34 @@ final class MysqlUserRepository implements UserRepository
         VALUES(:username, :email, :password, :birthday, :phone)
         QUERY;
         
+        $statement = $this->database->connection()->prepare($query);
+
+        $username = $user->getUsername();
+        $email = $user->email();
+        $password = $user->password();
+        $birthady = $user->getBirthday()->format(self::DATE_FORMAT);
+        $phone = $user->getPhone();
+
+        $statement->bindParam('username', $username, PDO::PARAM_STR);
+        $statement->bindParam('email', $email, PDO::PARAM_STR);
+        $statement->bindParam('password', $password, PDO::PARAM_STR);
+        $statement->bindParam('birthday', $birthady, PDO::PARAM_STR);
+        $statement->bindParam('phone', $phone, PDO::PARAM_STR);
+
+        $statement->execute();
+    }
+
+    public function savePendingUser(User $user): void {
+        // Check if user already exists.
+        if($this->emailExists($user)) {
+            throw new Exception('This email is already used!');
+        }
+
+        $query = <<<'QUERY'
+        INSERT INTO usersPending(username, email, password, birthday, phone)
+        VALUES(:username, :email, :password, :birthday, :phone)
+        QUERY;
+
         $statement = $this->database->connection()->prepare($query);
 
         $username = $user->getUsername();
