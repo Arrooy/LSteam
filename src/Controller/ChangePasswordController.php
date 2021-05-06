20d61bbe77a6779f51c08f1cf1a6e005c3f187af
Miquel Saula Mañosa <miquel.saula@students.salle.url.edu>
2021-05-07 00:15:02 +0200

    solved bugs

--- src/Controller/ChangePasswordController.php
+++ src/Controller/ChangePasswordController.php
@@ -72,16 +72,6 @@ final class ChangePasswordController
 
             // Hrefs de la base
             'profilePic' => (!isset($_SESSION['profilePic']) ? "" : $routeParser->urlFor('home') . $_SESSION['profilePic']),
-            'log_in_href' => $routeParser->urlFor('login'),
-            'log_out_href' => $routeParser->urlFor('logOut'),
-            'sign_up_href' => $routeParser->urlFor('register'),
-            'profile_href' => $routeParser->urlFor('profile'),
-            'home_href' => $routeParser->urlFor('home'),
-            'store_href' => $routeParser->urlFor('store'),
-            'friends_href' => $routeParser->urlFor('friends'),
-            'wallet_href' => $routeParser->urlFor('getWallet'),
-            'myGames_href' => $routeParser->urlFor('myGames'),
-            'wishlist_href' => $routeParser->urlFor('wishlist'),
         ]);
     }
 
